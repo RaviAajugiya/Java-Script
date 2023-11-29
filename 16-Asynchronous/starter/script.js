f6b3ed90f0ae1764836ce25fbf6aba2e19950a34
Aajugiya Ravi <aajugiyaravi@gmail.com>
2023-11-30 00:23:04 +0530

    modified:   16-Asynchronous/starter/script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -58,19 +58,103 @@ const countriesContainer = document.querySelector('.countries');
 
 
 // create promise
-const lottery = new Promise(function (resolve, reject) {
-    
-    setTimeout(function(){
-        if(Math.random() >= 0.5){
-            resolve('You WIN'); //by callig this we actually fullfill promise
-        }else{
-            reject('You lost')
+// const lottery = new Promise(function (resolve, reject) {
+
+//     setTimeout(function () {
+//         if (Math.random() >= 0.5) {
+//             resolve('You WIN'); //by callig this we actually fullfill promise
+//         } else {
+//             reject('You lost')
+//         }
+
+//     }, 2000);
+// });
+
+// lottery.then(res => console.log(res)).catch(e => console.error(e));
+
+
+// const loc = async function () {
+//     // more readable and 
+//     const res = await fetch('https://restcountries.com/v3.1/name/india');
+//     const data = await res.json()
+//     console.log(data);
+
+//     // both same 
+//     fetch('https://restcountries.com/v3.1/name/india')
+//         .then(res => console.log(res))
+
+// }
+
+// loc();
+
+const getJSON = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
         }
+        return await response.json();
+    } catch (error) {
+        throw new Error(`Fetch failed: ${error.message}`);
+    }
+};
 
-    },2000);
-});
+const get3country = async function (c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-lottery.then(res => console.log(res)).catch(e => console.error(e));
+        // console.log([data1.capital,data3.capital,data2.capital]);
+
+
+        const data = await Promise.all([
+            getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+            getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+            getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+        ]);
+        console.log(data.map(d => d[0].capital));
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+get3country('portugal', 'canada', 'tanzania')
+
+// combinators
+// Promise.all => accept arr of promise return all value
+// if one have error all will be rejected
+
+// promise.race => accept arr of promise and return first req data which executed oters are ignored
+// used when we want to reject all req after certain amount of time
+
+// promise.allSettled => never sort circuit(return alll rejected also )
+
+// promise.any => return first fullfilled promise ignore rejected once
+// race not ignore reject
 
 
 
+Promise.allSettled([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+
+Promise.any([
+    Promise.reject('ERROR'),
+    Promise.resolve('Success'),
+    Promise.resolve('Another success'),
+])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
